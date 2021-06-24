d16319b71e4ca7bd364dc0f4670ad354bfbbd793
Arpit Jain <arpitjain101992@gmail.com>
2021-06-24 19:30:23 +0530

    data collection api added

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-
+from starlette.routing import Host
+import uvicorn
 from client.api.api import api_router
 
 app = FastAPI()
-
-app.include_router(api_router)
+if __name__ == "__main__":
+    app.include_router(api_router)
+    uvicorn.run(app, host="0.0.0.0", port=7000)
